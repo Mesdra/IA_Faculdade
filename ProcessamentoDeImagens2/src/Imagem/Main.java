cc9166e4c2cb25395293f2242c38871aa2d38ace
vini <vini@DESKTOP-QA0JORM>
2018-03-23 00:33:03 -0300

    comit

---
+++ ProcessamentoDeImagens2/src/Imagem/Main.java
@@ -0,0 +1,11 @@
+package Imagem;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    LeituraImagem leitura = new LeituraImagem();
+    leitura.jButton1ActionPerformed();
+	}
+
+}
